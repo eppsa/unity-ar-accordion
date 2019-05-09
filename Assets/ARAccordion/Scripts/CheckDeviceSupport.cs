d8d2ca67b2ad4cda84d6d3fc435609b9d5f6dc3d
Jens Eraßmy <jens.erassmy@artcom.de>
2019-05-09 12:43:26 +0200

    Use AR Foundation Preview

---
+++ Assets/ARAccordion/Scripts/CheckDeviceSupport.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class CheckDeviceSupport : MonoBehaviour {
+    [SerializeField] ARSession m_Session;
+
+    IEnumerator Start() {
+        if ((ARSession.state == ARSessionState.None) ||
+            (ARSession.state == ARSessionState.CheckingAvailability))
+        {
+            yield return ARSession.CheckAvailability();
+        }
+
+        if (ARSession.state == ARSessionState.Unsupported)
+        {
+            // Start some fallback experience for unsupported devices
+            Debug.Log("Device is unsupported.");
+        }
+        else
+        {
+            // Start the AR session
+            m_Session.enabled = true;
+        }
+    }
+}
