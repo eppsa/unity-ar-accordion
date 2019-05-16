294710acb3d7860ced959dd601430ae165ce3339
tillgi <till.gilsbach@artcom.de>
2019-05-16 12:13:54 +0200

    Adjust infoPopup call functions

--- Assets/ARAccordion/Scripts/Accordion.cs
+++ Assets/ARAccordion/Scripts/Accordion.cs
@@ -24,14 +24,14 @@ public class Accordion : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetAxis("Mouse ScrollWheel") < 0) {
             if (step > 0) {
                 step--;
-                infoPopup.GetComponent<infoPopup>().UpdateInformation(step);
+                infoPopup.GetComponent<infoPopup>().SwitchLayer(step);
             }
         }
 
         if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetAxis("Mouse ScrollWheel") > 0) {
             if (step < tiles.Length) { 
                step++;
-               infoPopup.GetComponent<infoPopup>().UpdateInformation(step);
+               infoPopup.GetComponent<infoPopup>().SwitchLayer(step);
             }
         }
 
@@ -43,13 +43,13 @@ public class Accordion : MonoBehaviour
                 if (touch.position.x < 1000) {
                     if (step > 0) { 
                         step--;
-                        infoPopup.GetComponent<infoPopup>().UpdateInformation(step);
+                        infoPopup.GetComponent<infoPopup>().SwitchLayer(step);
 
                     }
                 } else {
                     if (step < tiles.Length) { 
                         step++;
-                        infoPopup.GetComponent<infoPopup>().UpdateInformation(step);
+                        infoPopup.GetComponent<infoPopup>().SwitchLayer(step);
 
                     }
                 }
