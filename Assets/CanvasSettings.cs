09218ba0e87f0d9e2145f5f8e25cdf84ce0ac9a0
tillgi <till.gilsbach@artcom.de>
2019-05-22 12:04:51 +0200

    Add basic movement functionality for canvas

---
+++ Assets/CanvasSettings.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasSettings : MonoBehaviour
+{
+    public void updateCanvasPosition(Vector3 newPosition) {
+        Debug.Log("update layer");
+        this.gameObject.transform.position = newPosition;
+    }
+}
