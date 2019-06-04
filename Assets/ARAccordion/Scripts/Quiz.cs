c775f8f83b36294fd53569f04c472183c036ff83
tillgi <till.gilsbach@artcom.de>
2019-06-04 18:58:03 +0200

    Add snap and win condition

--- Assets/ARAccordion/Scripts/Quiz.cs
+++ Assets/ARAccordion/Scripts/Quiz.cs
@@ -6,15 +6,32 @@ using UnityEngine.UI;
 [RequireComponent(typeof(Image))]
 public class Quiz : MonoBehaviour, IDragHandler, IDropHandler, IPointerEnterHandler, IPointerExitHandler
 {	
+	private GameObject detailPopup;
+	public Color32 heighlightCorrect;
+	public Color32 heightlightWrong;
 
-	public static GameObject activeTile;
+	private string correctAnswer = "Weisheit";
+	private GameObject activeTile;
+	private Vector3 tileStartPosition;
+
+
+	private void Start()
+	{
+		detailPopup = GameObject.Find("InformationContainer");
+		detailPopup.SetActive(false);
+	}
 
 	public void OnPointerEnter(PointerEventData eventData)
 	{
-		Debug.Log(eventData.pointerEnter);
 		if (activeTile == null)
 		{
 			activeTile = eventData.pointerEnter;
+			tileStartPosition = activeTile.transform.position;
+
+			if (activeTile.tag == "AnswerContainer")
+			{
+				activeTile.transform.localScale = new Vector3(1.5f,1.5f,1.5f);
+			}
 		}
 	}
 
@@ -22,25 +39,63 @@ public class Quiz : MonoBehaviour, IDragHandler, IDropHandler, IPointerEnterHand
 	{
 		Vector3 worldPoint;
         
-		if (RectTransformUtility.ScreenPointToWorldPointInRectangle(activeTile.GetComponent<RectTransform>(), eventData.position, eventData.pressEventCamera, out worldPoint))
+		if (activeTile != null)
 		{
-			if (activeTile.tag == "AnswerContainer")
+			if (RectTransformUtility.ScreenPointToWorldPointInRectangle(activeTile.GetComponent<RectTransform>(),
+																	eventData.position,
+																	eventData.pressEventCamera,
+																	out worldPoint))
 			{
-				activeTile.transform.position = worldPoint;
+				if (activeTile.tag == "AnswerContainer")
+				{
+					activeTile.transform.localScale = new Vector3(1,1,1);
+					activeTile.transform.position = worldPoint;
+				}
 			}
 		}
 	}
 
-	public void OnDrop(PointerEventData data)
+	public void OnDrop(PointerEventData eventData)
 	{
-		if (data.pointerEnter.tag == "DropArea")
+		if (eventData.pointerEnter.tag == "DropArea")
 		{
-			activeTile.transform.position = data.pointerEnter.transform.position;
+			activeTile.transform.position = eventData.pointerEnter.transform.position;
+			checkAnswer();		
+
+		}
+		else 
+		{
+			activeTile.transform.position = tileStartPosition;
 		}
 	}
 
 	public void OnPointerExit(PointerEventData eventData)
 	{
-		activeTile = null;
+		if (activeTile != null)
+		{
+			if (activeTile.tag == "AnswerContainer")
+			{
+				activeTile.transform.position = tileStartPosition;
+				activeTile.GetComponent<Image>().color = new Color (100,100,100);
+				activeTile.transform.localScale = new Vector3(1,1,1);
+			}
+			activeTile = null;
+		}
+	}
+
+	private void checkAnswer() 
+	{
+		string currentAnswer = activeTile.GetComponentInChildren<Text>().text;
+
+		if (currentAnswer == correctAnswer)
+		{
+			Debug.Log("Right");
+			activeTile.GetComponent<Image>().color = new Color (0,200,0);
+		}
+		else
+		{
+			Debug.Log("Wrong");
+			activeTile.GetComponent<Image>().color = new Color (200,0,0);
+		}
 	}
 }
