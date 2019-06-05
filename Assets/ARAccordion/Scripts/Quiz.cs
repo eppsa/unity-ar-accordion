0c2a98ec460fe401b5e81d260bc65dd4ea4ce000
tillgi <till.gilsbach@artcom.de>
2019-06-05 10:39:24 +0200

    Add fading functionality & refactor answer heighlighting

--- Assets/ARAccordion/Scripts/Quiz.cs
+++ Assets/ARAccordion/Scripts/Quiz.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
@@ -7,18 +8,23 @@ using UnityEngine.UI;
 public class Quiz : MonoBehaviour, IDragHandler, IDropHandler, IPointerEnterHandler, IPointerExitHandler
 {	
 	private GameObject detailPopup;
-	public Color32 heighlightCorrect;
-	public Color32 heightlightWrong;
+	private Color heighlightCorrect = new Color (0,200,0);
+	private Color heightlightWrong = new Color (200,0,0);
 
 	private string correctAnswer = "Weisheit";
 	private GameObject activeTile;
 	private Vector3 tileStartPosition;
 
+	bool fadeRunning;
+
+	bool answerGiven;
+
 
 	private void Start()
 	{
 		detailPopup = GameObject.Find("InformationContainer");
-		detailPopup.SetActive(false);
+		detailPopup.GetComponent<CanvasGroup>().alpha = 0;
+
 	}
 
 	public void OnPointerEnter(PointerEventData eventData)
@@ -59,6 +65,7 @@ public class Quiz : MonoBehaviour, IDragHandler, IDropHandler, IPointerEnterHand
 	{
 		if (eventData.pointerEnter.tag == "DropArea")
 		{
+			answerGiven = true;
 			activeTile.transform.position = eventData.pointerEnter.transform.position;
 			checkAnswer();		
 
@@ -73,8 +80,9 @@ public class Quiz : MonoBehaviour, IDragHandler, IDropHandler, IPointerEnterHand
 	{
 		if (activeTile != null)
 		{
-			if (activeTile.tag == "AnswerContainer")
+			if (activeTile.tag == "AnswerContainer" && !answerGiven)
 			{
+				Debug.Log("snapped away");
 				activeTile.transform.position = tileStartPosition;
 				activeTile.GetComponent<Image>().color = new Color (100,100,100);
 				activeTile.transform.localScale = new Vector3(1,1,1);
@@ -90,12 +98,38 @@ public class Quiz : MonoBehaviour, IDragHandler, IDropHandler, IPointerEnterHand
 		if (currentAnswer == correctAnswer)
 		{
 			Debug.Log("Right");
-			activeTile.GetComponent<Image>().color = new Color (0,200,0);
+			activeTile.GetComponent<Image>().color = heighlightCorrect;
+			StartCoroutine(Fade(0.0f,1.0f,0.7f));
+
 		}
 		else
 		{
 			Debug.Log("Wrong");
-			activeTile.GetComponent<Image>().color = new Color (200,0,0);
+			activeTile.GetComponent<Image>().color = heightlightWrong;
 		}
 	}
+
+	private IEnumerator Fade(float fadeFrom, float fadeTo, float duration)
+    {
+        fadeRunning = true;
+
+        float startTime = Time.time;
+        float currentDuration = 0.0f;
+        float progress = 0.0f;
+
+        while (true)
+        {
+            currentDuration = Time.time - startTime;
+            progress = currentDuration / duration;
+
+            if (progress <= 1.0f) {
+                detailPopup.GetComponent<CanvasGroup>().alpha = Mathf.Lerp(fadeFrom, fadeTo, progress);
+                yield return new WaitForEndOfFrame();
+            } else {
+                detailPopup.GetComponent<CanvasGroup>().alpha = fadeTo;
+                fadeRunning = false;
+                yield break;
+            }
+        }
+    }
 }
