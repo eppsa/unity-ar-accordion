d77a5302dae613d805f791166ead70700e358e0f
Jens Eraßmy <jens.erassmy@artcom.de>
2019-06-25 16:36:53 +0200

    Adjust positions when interacting

--- Assets/ARAccordion/Scripts/RotationWheel.cs
+++ Assets/ARAccordion/Scripts/RotationWheel.cs
@@ -9,19 +9,41 @@ public class RotationWheel : MonoBehaviour, IDragHandler
     private float minY;
     private float maxY;
 
+    void OnEnable() {
+
+    }
+
     void Start() {
         minY = wheelContainer.transform.position.y;
-        maxY = wheelContainer.GetComponent<RectTransform>().rect.height * transform.localScale.y;
+        // maxY = wheelContainer.GetComponent<RectTransform>().rect.height * transform.localScale.y;
+        maxY = wheelRectTransform.sizeDelta.y;
+
+        // Debug.Log(wheelRectTransform.rect.height);
+        Debug.Log(minY);
+        Debug.Log(maxY);
     }
 
     void IDragHandler.OnDrag(PointerEventData eventData)
     {
-        var newY = Mathf.Max(Mathf.Min(wheelContainer.transform.position.y + eventData.delta.y, maxY), minY);
+        // var newY = Mathf.Max(Mathf.Min(wheelContainer.transform.position.y + eventData.delta.y, maxY), minY);
 
-        wheelContainer.transform.position = new Vector3(
-            wheelContainer.transform.position.x, 
-            newY, 
-            wheelContainer.transform.position.z
+        var newLocalY = Mathf.Max(Mathf.Min(wheelContainer.transform.localPosition.y + eventData.delta.y, maxY), 0);
+        
+
+        // Debug.Log(newY);
+        Debug.Log(newLocalY);
+
+
+        // wheelContainer.transform.position = new Vector3(
+        //     wheelContainer.transform.position.x, 
+        //     newY, 
+        //     wheelContainer.transform.position.z
+        // );
+
+        wheelContainer.transform.localPosition = new Vector3(
+            wheelContainer.transform.localPosition.x, 
+            newLocalY, 
+            wheelContainer.transform.localPosition.z
         );
     }
 }
