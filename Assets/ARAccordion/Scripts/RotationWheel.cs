15deadeedb16b36c6e69b28619206b2c4a91e40f
Jens Eraßmy <jens.erassmy@artcom.de>
2019-06-25 15:26:40 +0200

    Add basic rotation wheel ui

---
+++ Assets/ARAccordion/Scripts/RotationWheel.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class RotationWheel : MonoBehaviour, IDragHandler
+{
+    [SerializeField] private GameObject wheelContainer;
+    [SerializeField] private RectTransform wheelRectTransform;
+
+    private float minY;
+    private float maxY;
+
+    void Start() {
+        minY = wheelContainer.transform.position.y;
+        maxY = wheelContainer.GetComponent<RectTransform>().rect.height * transform.localScale.y;
+    }
+
+    void IDragHandler.OnDrag(PointerEventData eventData)
+    {
+        var newY = Mathf.Max(Mathf.Min(wheelContainer.transform.position.y + eventData.delta.y, maxY), minY);
+
+        wheelContainer.transform.position = new Vector3(
+            wheelContainer.transform.position.x, 
+            newY, 
+            wheelContainer.transform.position.z
+        );
+    }
+}
