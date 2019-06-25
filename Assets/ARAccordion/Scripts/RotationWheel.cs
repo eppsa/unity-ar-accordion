df62478cea9407fee81f9bcc01e0034c58785a25
Jens Eraßmy <jens.erassmy@artcom.de>
2019-06-25 18:56:09 +0200

    Snap into step position when dropped

--- Assets/ARAccordion/Scripts/RotationWheel.cs
+++ Assets/ARAccordion/Scripts/RotationWheel.cs
@@ -1,8 +1,9 @@
 ﻿using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class RotationWheel : MonoBehaviour, IDragHandler
+public class RotationWheel : MonoBehaviour, IDragHandler, IDropHandler
 {
     [SerializeField] private GameObject wheelContainer;
     [SerializeField] private GameObject wheelElementPrefab;
@@ -11,11 +12,15 @@ public class RotationWheel : MonoBehaviour, IDragHandler
     private float maxY;
 
     private int maxSteps = 3;
+    private Vector3 nextStepLocalPosition;
 
+    private bool dragging = false;
 
     void IDragHandler.OnDrag(PointerEventData eventData)
     {
-        var newLocalY = Mathf.Max(Mathf.Min(wheelContainer.transform.localPosition.y + eventData.delta.y, maxY), 0);
+        dragging = true;
+
+        float newLocalY = Mathf.Max(Mathf.Min(wheelContainer.transform.localPosition.y + eventData.delta.y, maxY), 0);
 
         wheelContainer.transform.localPosition = new Vector3(
             wheelContainer.transform.localPosition.x, 
@@ -24,12 +29,36 @@ public class RotationWheel : MonoBehaviour, IDragHandler
         );
     }
 
+    void IDropHandler.OnDrop(PointerEventData eventData)
+    {
+        float newLocalY = Mathf.Max(Mathf.Min(wheelContainer.transform.localPosition.y + eventData.delta.y, maxY), 0);
+
+        int step = Mathf.RoundToInt(newLocalY / wheelElementPrefab.GetComponent<RectTransform>().sizeDelta.y);
+
+        nextStepLocalPosition = new Vector3(
+            wheelContainer.transform.localPosition.x, 
+            step * wheelElementPrefab.GetComponent<RectTransform>().sizeDelta.y, 
+            wheelContainer.transform.localPosition.z
+        );
+
+        dragging = false;
+    }
+
+    void Start() {
+        nextStepLocalPosition = wheelContainer.transform.localPosition;
+    }
+
+    void Update() {
+        if (!dragging) {
+            wheelContainer.transform.localPosition = Vector3.MoveTowards(wheelContainer.transform.localPosition, nextStepLocalPosition, Time.deltaTime * 200f);
+        }
+    }
+
     internal void Init(int maxSteps)
     {
         this.maxSteps = maxSteps;
 
-        for (int i = 0; i < maxSteps; i++)
-        {
+        for (int i = 0; i < maxSteps; i++) {
             GameObject wheelElement = Instantiate(wheelElementPrefab);   
             wheelElement.transform.SetParent(wheelContainer.transform, false); 
 
