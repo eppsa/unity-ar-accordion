0ffaedc8f00e30941d6d595bf1fea8eea51a27e6
Jens Eraßmy <jens.erassmy@artcom.de>
2019-07-02 17:28:16 +0200

    Clean up

--- Assets/ARAccordion/Scripts/InfoPopup.cs
+++ Assets/ARAccordion/Scripts/InfoPopup.cs
@@ -1,10 +1,6 @@
-using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.XR.ARFoundation;
-using Model;
 
 public class InfoPopup : MonoBehaviour
 {
