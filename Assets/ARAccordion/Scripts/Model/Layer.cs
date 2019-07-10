81bca011761d68663e468d5ebbf419e92be6f470
till gilsbach <till.gilsbach@artcom.de>
2019-07-10 18:41:13 +0200

    Add layer id to content

--- Assets/ARAccordion/Scripts/Model/Layer.cs
+++ Assets/ARAccordion/Scripts/Model/Layer.cs
@@ -4,6 +4,7 @@ namespace Model
 {
     public class Layer
     {
+        public int id { get; set; }
         public string information { get; set; }
         public List<Question> questions { get; set; }
 
