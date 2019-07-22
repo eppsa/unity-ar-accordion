aab455dfbcbb2908500005c7522a3f5bdc751111
Jens Eraßmy <jens.erassmy@artcom.de>
2019-07-22 12:06:16 +0200

    Clean up

--- Assets/ARAccordion/Scripts/Accordion.cs
+++ Assets/ARAccordion/Scripts/Accordion.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
 using Model;
-using System;
 using System.Collections.Generic;
 
 public class Accordion : MonoBehaviour
