c8939970be379dd8dd9ffc66de6ed7e980e1b238
Jens Eraßmy <jens.erassmy@artcom.de>
2019-07-22 12:07:08 +0200

    Clean up

--- Assets/ARAccordion/Scripts/DebugText.cs
+++ Assets/ARAccordion/Scripts/DebugText.cs
@@ -1,23 +1,10 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
 public class DebugText : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void UpdateValue(float sliderValue)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    public void UpdateValue(float sliderValue) {
         GetComponent<Text>().text = sliderValue.ToString();
     }
 }
