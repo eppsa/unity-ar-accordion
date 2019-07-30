28465da8a904a4a95f36ef70cb7f37bee5d31aed
till gilsbach <till.gilsbach@artcom.de>
2019-07-30 12:36:18 +0200

    Fade screen in and out

--- Assets/ARAccordion/Scripts/StartScreenCanvas.cs
+++ Assets/ARAccordion/Scripts/StartScreenCanvas.cs
@@ -1,8 +1,21 @@
 ﻿using UnityEngine;
+using System.Collections;
+
 
 public class StartScreenCanvas : MonoBehaviour
 {
     private Controller Controller;
+
+    private bool isFadeing;
+    private float duration = 0.7f;
+    private float prepareDelay = 0.5f;
+
+
+
+    public void OnEnable()
+    {
+        StartCoroutine(Fade(0.0f, 1.0f, duration));
+    }
 
     void Start()
     {
@@ -11,7 +24,45 @@ public class StartScreenCanvas : MonoBehaviour
 
     public void OnStartButton()
     {
+        StartCoroutine(PrepareMainView());
+    }
+
+    private IEnumerator PrepareMainView()
+    {
+
+        StartCoroutine(Fade(1.0f, 0.0f, duration));
+        while (isFadeing) {
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(prepareDelay);
+
         this.transform.gameObject.SetActive(false);
         Controller.OnStart();
     }
+
+    private IEnumerator Fade(float fadeFrom, float fadeTo, float duration)
+    {
+        isFadeing = true;
+
+        float startTime = Time.time;
+        float currentDuration = 0.0f;
+        float progress = 0.0f;
+
+        while (true) {
+            currentDuration = Time.time - startTime;
+            progress = currentDuration / duration;
+
+            if (progress <= 1.0f) {
+                GetComponent<CanvasGroup>().alpha = Mathf.Lerp(fadeFrom, fadeTo, progress);
+                yield return new WaitForEndOfFrame();
+            } else {
+                this.GetComponent<CanvasGroup>().alpha = fadeTo;
+                isFadeing = false;
+                yield break;
+            }
+        }
+    }
 }
+
+
