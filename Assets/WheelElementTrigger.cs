d383fb3cf064aabc211433b0899c6283b59c66f4
till gilsbach <till.gilsbach@artcom.de>
2019-08-08 19:25:57 +0200

    Mute rotation wheel sound at first

--- Assets/WheelElementTrigger.cs
+++ Assets/WheelElementTrigger.cs
@@ -8,10 +8,19 @@ public class WheelElementTrigger : MonoBehaviour
 {
     private AudioSource RotationWheelSound;
 
-    void Start()
+    void OnEnable()
     {
         RotationWheelSound = GameObject.Find("Sounds/RotationWheel").GetComponent<AudioSource>();
+        RotationWheelSound.mute = true;
+
+        Invoke("Unmute", 0.1f);
     }
+
+    void Unmute()
+    {
+        RotationWheelSound.mute = false;
+    }
+
     public void OnTriggerEnter2D(Collider2D obj)
     {
         RotationWheelSound.Play();
