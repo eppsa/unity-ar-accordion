0c3a58351f8e789403288f2a54f7310fadfe0fb6
till gilsbach <till.gilsbach@artcom.de>
2019-08-14 11:48:36 +0200

    Quiz, result container: Correct button highlight functionality
    - button gets just highlighted by "tinting" which just works on a white button
    - helper makes sure button "ground color" is white when tinting is necessary

---
+++ Assets/ARAccordion/Scripts/ButtonHighlightHelper.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ButtonHighlightHelper : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        this.GetComponent<Image>().color = new Color(1, 1, 1, 1);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        this.GetComponent<Image>().color = new Color(0, 0, 0, 1);
+    }
+}
